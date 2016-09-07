91096235ba96f9370c087a298ac6634b5fec9723
John Wang <jwang@remi.ai>
2016-09-07 21:48:57 +1000

    Update spoonacularAPI.py

--- spoonacularAPI.py
+++ spoonacularAPI.py
@@ -11,56 +11,32 @@ CRED = {
 
 
 def match_input(input_): #this function matches the input from Remi to the available options in spoonacular
-	cuisine_options = ['african', 'chinese', 'japanese', 'korean', 'vietnamese', 'thai', 'indian', 'british', 'irish', 'french', 'italian', 'mexican', 'spanish', 
-		'middle eastern', 'jewish', 'american', 'cajun', 'southern', 'greek', 'german', 'nordic', 'eastern european', 'caribbean', 'latin american']
+
 	diet_options = ['pescetarian', 'lacto vegetarian', 'ovo vegetarian', 'vegan', 'paleo', 'primal', 'vegetarian']
-	intolerance_options = ['dairy', 'egg', 'gluten', 'peanut', 'sesame', 'seafood', 'shellfish', 'soy', 'sulfite', 'tree nut', 'wheat']
-	type_options = ['main course', 'side dish', 'dessert', 'appetizer', 'salad', 'bread', 'breakfast', 'soup', 'beverage', 'sauce', 'drink']
+	intolerance_options = ['dairy', 'egg', 'gluten', 'peanut', 'sesame', 'seafood', 'shellfish', 'soy', 'sulfite', 'treenut', 'wheat']
+
+
+	# type_options = ['main course', 'side dish', 'dessert', 'appetizer', 'salad', 'bread', 'breakfast', 'soup', 'beverage', 'sauce', 'drink']
+	# cuisine_options = ['african', 'chinese', 'japanese', 'korean', 'vietnamese', 'thai', 'indian', 'british', 'irish', 'french', 'italian', 'mexican', 'spanish', 
+	# 	'middle eastern', 'jewish', 'american', 'cajun', 'southern', 'greek', 'german', 'nordic', 'eastern european', 'caribbean', 'latin american']
 
 	matched_diet = []
-	
-	if input_['diet']:
-		for variable in input_['diet']:
-			match = fp.extractOne(variable, diet_options)
-			if match[1] > 75:
-				matched_diet.append(match[0])
-				input_['diet'].remove(variable)
-
 	matched_intolerance = []
-	if input_['intolerance']:
-		for variable in input_['intolerance']:
-			match = fp.extractOne(variable, intolerance_options)
-			if match[1] > 75:
-				matched_intolerance.append(match[0])
-				input_['intolerance'].remove(variable)
 
-	final_query_list = []
-	if input_['cuisine']:
-		match = fp.extractOne(input_['cuisine'], cuisine_options)
-		if match[1] > 75:
-			input_['cuisine'] = match[0]
-		else:
-			final_query_list.append(input_['cuisine'])
-			input_['cuisine'] = None
+	for var in input_['diet']:
+		m_diet = fp.extractOne(var, diet_options)
+		m_intolerance = fp.extractOne(var, intolerance_options)
 
-	if input_['type']:
-		match = fp.extractOne(input_['type'], type_options)
-		if match[1] > 75:
-			input_['type'] = match[0]
-		else:
-			final_query_list.append(input_['type'])
-			input_['type'] = None
-
-
-	final_query_list = input_['intolerance'] + input_['diet'] + [input_['query']]
-	if input_['query']:
-		input_['query'] = ' '.join(final_query_list)
+		if m_diet[1] > 75:
+			matched_diet.append(m_diet[0])
+		if m_intolerance[1] > 75:
+			matched_intolerance.append(m_intolerance[0])
+		if m_diet[1] <= 75 and m_intolerance[1]<= 75:
+			input_['food'] = var+' '+input_['food']
 
 	input_.update({'diet': ','.join(matched_diet), 'intolerance': ','.join(matched_intolerance)})
 
-	input_['excludeIngredients'] = ','.join(input_['excludeIngredients'])
-	input_['includeIngredients'] =','.join(input_['includeIngredients'])
-
+	input_['Ingredients'] =','.join(input_['Ingredients'])
 	return input_
 
 def spoonacular_API(matched_input):
@@ -68,46 +44,20 @@ def spoonacular_API(matched_input):
 	headers = {}
 	headers.update(CRED)
 
-	if matched_input['query']:
-		endpoint = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex'
-		search = { #search content with some 4 default values
-			'number' : 10,	#number of results
-			'ranking' : 1, #Whether to maximize used ingredients (1) or minimize missing ingredients (2) first.
-			'offset' : 0, #the number of results to skip
+	endpoint = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex'
+	query = {
+		'query': matched_input['food'],
+		'includeIngredients': matched_input['ingredients'],
+		'diet': matched_input['diet'],
+		'intolerance': matched_input['intolerance'],
+		'number' : 10,	#number of results
+		'ranking' : 1, #Whether to maximize used ingredients (1) or minimize missing ingredients (2) first.
+		'offset' : 0, #the number of results to skip
+	}
 
-			'cuisine' : None,
-			'diet' : None,
-			'excludeIngredients' : None,
-			'fillIngredients': None,
-			'includeIngredients' : None,
-			'intolerance' : None,
-			'limitLicense' : False, #Whether the recipes should have an open license that allows for displaying with proper attribution.
-			'maxCalories' : None,
-			'maxCarbs' : None,
-			'maxFat' : None,
-			'maxProtein' : None,
-			'minClories' : None,
-			'minCarbs' : None,
-			'minFat' : None,
-			'minProtein' : None,
-			'type' : None,
-		}
-		search.update(matched_input)
-
-	elif matched_input['includeIngredients']:
-		endpoint = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients'
-		search = {
-			'ingredients' : matched_input['includeIngredients'],
-			'fillIngredients': False,
-			'limitLicense': False,
-			'number': 10,
-			'ranking': 1,
-		}
-
-
-
-	response = requests.get(endpoint, headers = headers, params = search)
+	response = requests.get(endpoint, headers = headers, params = query)
 	return json.loads(response.text)
+
 
 def get_nutrition(id_):
 	headers = {}
@@ -134,26 +84,36 @@ def get_instruction(id_):
 if __name__ == '__main__':
 	#sample input
 	input_ = {
-		'query' : None,
+		'food' : 'chicken burger',
 
-		'diet' : ['vegan'],
-		'intolerance' : ['peanut'],
+		'diet' : ['vegan','no peanut','no watermalon'],
+		'Ingredients' : ['onions', 'lettuce'],
 
-		'excludeIngredients' : ['coconut'],
-		'includeIngredients' : ['onions', 'lettuce'],
-
-		'cuisine' : 'american',
-		'type': 'main course',
+		# 'cuisine' : 'american',
+		# 'type': 'main course',
+		# 'fillIngredients': None,
+		# 'limitLicense' : False, #Whether the recipes should have an open license that allows for displaying with proper attribution.
+		# 'maxCalories' : None,
+		# 'maxCarbs' : None,
+		# 'maxFat' : None,
+		# 'maxProtein' : None,
+		# 'minClories' : None,
+		# 'minCarbs' : None,
+		# 'minFat' : None,
+		# 'minProtein' : None,
 	}
-	data = spoonacular_API(match_input(input_))
-	try:
-		for result in data['results']:
-			print ('title: %s\nresult id: %s, \nused ingredients count: %s\nmissed ingredients count: %s' %(result['title'],result['id'],result['usedIngredientCount'],result['missedIngredientCount']))
-			print ('====================')
-	except TypeError:
-		for result in data:
-			print ('title: %s\nresult id: %s, \nused ingredients count: %s\nmissed ingredients count: %s' %(result['title'],result['id'],result['usedIngredientCount'],result['missedIngredientCount']))
-			print ('====================')
+
+	print match_input(input_)
+	# data = spoonacular_API(match_input(input_))
+
+	# try:
+	# 	for result in data['results']:
+	# 		print ('title: %s\nresult id: %s, \nused ingredients count: %s\nmissed ingredients count: %s' %(result['title'],result['id'],result['usedIngredientCount'],result['missedIngredientCount']))
+	# 		print ('====================')
+	# except TypeError:
+	# 	for result in data:
+	# 		print ('title: %s\nresult id: %s, \nused ingredients count: %s\nmissed ingredients count: %s' %(result['title'],result['id'],result['usedIngredientCount'],result['missedIngredientCount']))
+	# 		print ('====================')
 
 
 
